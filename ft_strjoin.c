081502736f3c5679d33c4ce5e1cb6c22b9a48b6e
fabbbiodc <162996736+fabbbiodc@users.noreply.github.com>
2024-04-30 14:59:53 +0200

    strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fdi-cecc <fdi-cecc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/30 14:27:34 by fdi-cecc          #+#    #+#             */
+/*   Updated: 2024/04/30 14:49:47 by fdi-cecc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*new;
+	size_t	i;
+	size_t	k;
+
+	if (!s1 || !s2)
+		return (NULL);
+	i = ft_strlen(s1);
+	k = ft_strlen(s2);
+	new = malloc(((i + k + 1)) * (sizeof(char)));
+	if (!new)
+		return (NULL);
+	i = 0;
+	k = 0;
+	while (s1[i])
+	{
+		new[i] = s1[i];
+		i++;
+	}
+	while (s2[k])
+	{
+		new[i + k] = s2[k];
+		k++;
+	}
+	new[i + k] = '\0';
+	return (new);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: fdi-cecc <fdi-cecc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 15:19:37 by fdi-cecc          #+#    #+#             */
-/*   Updated: 2024/04/30 12:46:40 by fdi-cecc         ###   ########.fr       */
+/*   Updated: 2024/04/30 14:40:35 by fdi-cecc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@
 # include <stdio.h>
 # include <stdlib.h>
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
 size_t	ft_strlen(const char *str);
 void	*ft_memset(void *str, int c, size_t n);
 void	*ft_bzero(void *str, size_t n);
@@ -29,17 +29,18 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dest, char const *src, size_t n);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
-int	ft_toupper(int c);
-int	ft_tolower(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 char	*ft_strchr(const char *str, int c);
 char	*ft_strrchr(const char *str, int c);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
-int ft_memcmp(const void *s1, const void *s2, size_t n);
+int 	ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *hay, const char *needle, size_t len);
-int	ft_atoi(const char *str);
-void *ft_calloc(size_t count, size_t size);
+int		ft_atoi(const char *str);
+void 	*ft_calloc(size_t count, size_t size);
 char	*ft_strdup(const char *str);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
