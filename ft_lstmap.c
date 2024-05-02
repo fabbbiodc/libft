bfeb6524c5f813f8a519e40b31e25da22527736b
Fabio Di Cecca <fdi-cecc@c4b3c6.42berlin.de>
2024-05-02 14:09:14 +0200

    strict passed

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: fdi-cecc <fdi-cecc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 02:29:47 by fdi-cecc          #+#    #+#             */
-/*   Updated: 2024/05/02 12:58:58 by fdi-cecc         ###   ########.fr       */
+/*   Updated: 2024/05/02 14:08:16 by fdi-cecc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,19 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
 	t_list	*new_node;
+	void	*set;
 
 	if (!lst || !f || !del)
 		return (NULL);
-	new_list = ft_lstnew(f(lst->content));
-	if (!new_list)
-		return (NULL);
+	new_list = NULL;
 	while (lst)
 	{
-		new_node = ft_lstnew(f(lst->content));
+		set = f(lst->content);
+		new_node = ft_lstnew(set);
 		if (!new_node)
 		{
-			ft_lstclear(&new_list, del);
+			del(set);
+			ft_lstclear(&new_list, (*del));
 			return (NULL);
 		}
 		ft_lstadd_back(&new_list, new_node);
